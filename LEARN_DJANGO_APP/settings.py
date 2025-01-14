fecc7b6cb31ba00641d3b4a071eaf1ec661aaeb1
Pachito <ankunskiy@mail.ru>
2025-01-14 14:34:06 +0300

    На этом этапе закончен поиск. 8 часов 56 минут позади

--- LEARN_DJANGO_APP/settings.py
+++ LEARN_DJANGO_APP/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
+
     'main',
     'goods',
     "debug_toolbar",
@@ -79,8 +81,12 @@ WSGI_APPLICATION = 'LEARN_DJANGO_APP.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'home',
+        'USER': 'home', 
+        'PASSWORD': 'home',
+        'HOST':'localhost',
+        'PORT': '5432',
     }
 }
 
